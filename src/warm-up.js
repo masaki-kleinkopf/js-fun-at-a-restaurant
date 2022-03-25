0c5b4ac7cfda0799569051c90a01bf5d6b602190
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-03-24 17:00:06 -0700

    initial commit

--- src/warm-up.js
+++ src/warm-up.js
@@ -12,20 +12,22 @@
 // var food = "Ice Cream";
 // var type = "Dessert";
 // var inStock = 10
+var food = "pizza"
+var type = "entree"
+var inStock = 5
 
 
-
-// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
+// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make
 // the inStock count double what it currently is.
+inStock *= 2
 
-
-// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
+// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack!
 // Use a built in math operator to decrement the inStock count by 1.
-
+inStock -= 1
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
-
+var isFavorite = true
 
 
 // Create a new variable named "shouldOrderMore", and use a comparison operator to
@@ -33,16 +35,20 @@
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
-
+var shouldOrderMore === true
 
 //------------------------- Conditionals -------------------------//
 
 
 
 // Express the following in code: If the variable "stockCount" is greater or equal to 1, log
-// the statement 'I'm going to eat some [food]!', otherwise, log the statement 
+// the statement 'I'm going to eat some [food]!', otherwise, log the statement
 // 'We need to get some more [food].'
-
+if (inStock >= 1){
+  console.log(`I'm going to eat some ${food}`)
+} else {
+  console.log(`We need to get some more ${food}`)
+}
 
 
 // Express the following in code: If the variable "isFavorite" is true,
