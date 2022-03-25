e217d1853a42fd8792147dfa3bee6d5f2731f828
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-03-24 18:18:56 -0700

    Add iteration 2

--- src/order.js
+++ src/order.js
@@ -1,7 +1,48 @@
 
+function takeOrder(order, orderList){
+  if(orderList.length < 3) {
+  return orderList.push(order)
+};
+};
+
+function refundOrder(orderNum, orderList){
+  for (var i = 0; i<orderList.length; i++){
+    if (orderNum === orderList[i].orderNumber){
+      orderList.splice(i,1);
+      }
+  }
+}
+
+function listItems(orders){
+  var orderNames = "";
+  for (var i = 0; i < orders.length; i++){
+    if (i === orders.length -1){
+      orderNames +=`${orders[i].item}`
+    }else{
+    orderNames += `${orders[i].item}, `
+  }
+  }
+  return orderNames
+}
+
+function searchOrder(ordersList, order){
+  var trueFalse = 0
+  for (var i = 0; i<ordersList.length;i++){
+    if (ordersList[i].item === order){
+      trueFalse++
+    }
+  };
+  if (trueFalse === 1){
+    return true;
+  }else {
+    return false;
+  }
+};
+//wondering how to refactor this...
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
